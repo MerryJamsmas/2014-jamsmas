cbce593d33548b7a38b81d2fb9b55cf875e9732d
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2015-01-02 02:16:47 -0700

    Added HelloWorld script.

---
+++ unity/Assets/Scripts/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestClass : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+        Debug.Log("Hello World!");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+}
