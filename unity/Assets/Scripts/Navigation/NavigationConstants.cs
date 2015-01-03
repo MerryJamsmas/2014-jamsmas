667a2a89f84d3ef04936c904bec759d1d1b32f37
dhuntley <supaklutz@hotmail.com>
2015-01-02 18:04:24 -0700

    Camera scroll limits and stub for NavigationShipController

--- unity/Assets/Scripts/Navigation/NavigationConstants.cs
+++ unity/Assets/Scripts/Navigation/NavigationConstants.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class NavigationConstants : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	[SerializeField]
+	private const float m_mapEdgeLeft = -2;
+
+	[SerializeField]
+	private const float m_mapEdgeRight = 2;
+
+	[SerializeField]
+	private const float m_mapEdgeTop = 2;
+
+	[SerializeField]
+	private const float m_mapEdgeBottom = -2;
+
+	public static float mapEdgeLeft { get { return m_mapEdgeLeft; } }
+	public static float mapEdgeRight { get { return m_mapEdgeRight;} }
+	public static float mapEdgeTop { get { return m_mapEdgeTop;} }
+	public static float mapEdgeBottom { get { return m_mapEdgeBottom;} }
 }
