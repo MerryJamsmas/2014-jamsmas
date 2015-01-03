5d02c2051f59d39a866d7ac70f681c619055f85e
dhuntley <supaklutz@hotmail.com>
2015-01-03 01:10:56 -0700

    Adding Moving of Ship

--- unity/Assets/Scripts/Navigation/NavigationConstants.cs
+++ unity/Assets/Scripts/Navigation/NavigationConstants.cs
@@ -3,20 +3,14 @@ using System.Collections;
 
 public class NavigationConstants : MonoBehaviour {
 
-	[SerializeField]
-	private const float m_mapEdgeLeft = -2;
-
-	[SerializeField]
-	private const float m_mapEdgeRight = 2;
-
-	[SerializeField]
-	private const float m_mapEdgeTop = 2;
-
-	[SerializeField]
-	private const float m_mapEdgeBottom = -2;
-
-	public static float mapEdgeLeft { get { return m_mapEdgeLeft; } }
-	public static float mapEdgeRight { get { return m_mapEdgeRight;} }
-	public static float mapEdgeTop { get { return m_mapEdgeTop;} }
-	public static float mapEdgeBottom { get { return m_mapEdgeBottom;} }
+	public static float m_mapEdge = 20;
+	
+	// These constants represent the bounds of the area in the scene
+	// in which we allow GameObjects to exist.  If a GameObject travels
+	// beyond these bounds, we Destroy it, and reinstantiate it if it needs
+	// to move back inside this area.
+	public static float mapEdgeLeft { get { return -m_mapEdge; } }
+	public static float mapEdgeRight { get { return m_mapEdge;} }
+	public static float mapEdgeTop { get { return m_mapEdge;} }
+	public static float mapEdgeBottom { get { return -m_mapEdge;} }
 }
--- unity/Assets/Scripts/Navigation/NavigationController.cs
+++ unity/Assets/Scripts/Navigation/NavigationController.cs
@@ -3,13 +3,72 @@ using System.Collections;
 
 public class NavigationController : MonoBehaviour {
 
+	public float cameraPanSpeed;
+	private Camera m_camera;
 	// Use this for initialization
 	void Start () {
-	
+		GameObject cameraGameObject = GameObject.FindGameObjectWithTag ("NavigationCamera");
+		if (cameraGameObject == null) {
+			Debug.Log ("Unable to find camera game object in navigation controller");
+		}
+		else{
+			m_camera = cameraGameObject.camera;
+			if(m_camera == null){
+				Debug.Log ("Unable to extract camera object in navigation controller");
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 objective = m_camera.ScreenToWorldPoint(Input.mousePosition);
+			GameObject shipGameObject = GameObject.FindGameObjectWithTag ("NavigationShip");
+			MoveShip (shipGameObject.transform.position.x - objective.x, shipGameObject.transform.position.y - objective.y);
+		}
+		else{
+			MoveCamera (cameraPanSpeed * Input.GetAxis("Horizontal"), cameraPanSpeed * Input.GetAxis("Vertical"));
 	
+		}
+	}
+
+	private void MoveCamera(float deltaX, float deltaY) {
+		TranslatePoiGameObjectsInScene (deltaX, deltaY);
+		TranslateShipGameObjectInScene (deltaX, deltaY);
+	}
+
+	private void MoveShip(float deltaX, float deltaY){
+		TranslatePoiGameObjectsInScene (deltaX, deltaY);
+	}
+
+	private void TranslateShipGameObjectInScene(float deltaX, float deltaY) {
+		GameObject shipGameObject = GameObject.FindGameObjectWithTag ("NavigationShip");
+		if (shipGameObject != null) {
+			TranslateGameObjectInScene (shipGameObject, deltaX, deltaY);
+		}
+	}
+
+	private void TranslatePoiGameObjectsInScene(float deltaX, float deltaY) {
+		GameObject[] poiGameObjects = GameObject.FindGameObjectsWithTag ("NavigationPOI");
+		foreach (GameObject poiGameObject in poiGameObjects) {
+			TranslateGameObjectInScene(poiGameObject, deltaX, deltaY);
+		}
+	}
+
+	private void TranslateGameObjectInScene(GameObject gObject, float deltaX, float deltaY) {
+		Vector3 position = gObject.transform.position;
+		
+		// Apply translation to gameobject to simulate camera movement
+		position.x += deltaX;
+		position.y += deltaY;
+		gObject.transform.position = position;
+		
+		// Destroy game object if outside of bounds of map area
+		if (position.x > NavigationConstants.mapEdgeRight || position.x < NavigationConstants.mapEdgeLeft 
+		    || position.y > NavigationConstants.mapEdgeTop || position.y < NavigationConstants.mapEdgeBottom)
+		{
+			// TODO: Make sure that destroying a gameobject in this iterator doesn't create problems
+			GameObject.Destroy(gObject);
+		}
 	}
 }
