a0e8887d59e9ba371fae5d0482ac0cfe8e134b36
dhuntley <supaklutz@hotmail.com>
2015-01-02 17:23:52 -0700

    Including .meta files in repository and ignoring .sln and .csproj

---
+++ unity/Assets/Scripts/Navigation/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public float m_panSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 cameraPosition = this.transform.position;
+
+		if (Input.GetAxis ("Horizontal") > 0) {
+			// Pan camera right
+			cameraPosition.x += m_panSpeed;
+		}
+		else if (Input.GetAxis ("Horizontal") < 0) {
+			// Pan camera left
+			cameraPosition.x -= m_panSpeed;
+		}
+
+		if (Input.GetAxis ("Vertical") > 0) {
+			// Pan camera up
+			cameraPosition.y += m_panSpeed;
+		}
+		else if (Input.GetAxis ("Vertical") < 0) {
+			// Pan camera down
+			cameraPosition.y -= m_panSpeed;
+		}
+		this.transform.position = cameraPosition;
+	}
+}
