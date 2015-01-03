71d5d000bb9a9571d8b7888e63589152e20155a9
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2015-01-03 00:45:44 -0700

    Asteroid Fields v1.0

---
+++ unity/Assets/Scripts/Navigation/Asteroid.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Asteroid
+{
+    public GameObject me;
+    public Vector3 pos;
+    public Quaternion rot;
+    float size;
+    
+    public Asteroid()
+    {
+        pos = new Vector3(0,0,0);
+        size = 1.0f;
+        rot = Quaternion.identity;
+    }
+
+    public Asteroid(float x, float y, float r, float phi)
+    {
+        pos = new Vector3(x, y, 0.0f);
+        size = r;
+        rot = Quaternion.AngleAxis(phi, Vector3.forward);
+    }
+
+    public void instantiate(GameObject astPreFab)
+    {
+        me = astPreFab;
+        me.transform.localScale = new Vector3(size, size, size);
+    }
+
+    public void destroy()
+    {
+        me = null;
+    }
+}
--- unity/Assets/Scripts/Navigation/NavigationController.cs
+++ unity/Assets/Scripts/Navigation/NavigationController.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class NavigationController : MonoBehaviour {
+public class NavigationController : MonoBehaviour 
+{
+
+    List<AsteroidField> allAsteroidFields;
+
+    public GameObject Asteroid;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start() 
+    {
+        makeAsteroidFields();
+        instantiateAllAsteroids();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+    {
 	
 	}
+
+    void makeAsteroidFields()
+    {
+        AsteroidField a;
+        allAsteroidFields = new List<AsteroidField>();
+
+        a = new AsteroidField(10.0f, 10.0f, 5.0f, 20);
+        allAsteroidFields.Add(a);
+        
+        a = new AsteroidField(-5.0f, 5.0f, 3.0f, 10);
+        allAsteroidFields.Add(a);
+        
+        a = new AsteroidField(0.0f, -10.0f, 8.0f, 200);
+        allAsteroidFields.Add(a);
+    }
+
+    void instantiateAllAsteroids()
+    {
+        foreach(AsteroidField af in allAsteroidFields)
+            foreach(Asteroid a in af.allAsteroids)
+                a.instantiate(Instantiate(Asteroid, a.pos, a.rot) as GameObject);
+    }
 }
