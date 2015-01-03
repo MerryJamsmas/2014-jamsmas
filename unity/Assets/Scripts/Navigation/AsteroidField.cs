29bf43e9ee206c7c2a3d15b1392b7354de39ffc1
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2015-01-02 23:19:32 -0700

    AsteroidField makes an Asteroid Field

--- unity/Assets/Scripts/Navigation/AsteroidField.cs
+++ unity/Assets/Scripts/Navigation/AsteroidField.cs
@@ -4,13 +4,15 @@ using System.Collections.Generic;
 
 public class AsteroidField : MonoBehaviour 
 {
-    public Transform Asteroid;
+    public GameObject Asteroid;
+    //Vector3 pos;
+    //float size;
 
 	// Use this for initialization
 	void Start () 
 	{
         Debug.Log("Loading Asteroid Field...");
-        Instantiate(Asteroid, new Vector3(1,1,0), Quaternion.identity);
+        MakeAnAsteroidField(10.0F, 10.0F, 100, 9.0F);
         Debug.Log("Asteroid Field Loaded.");
 	}
 	
@@ -18,4 +20,33 @@ public class AsteroidField : MonoBehaviour
 	void Update () 
 	{
 	}
+
+    void MakeAnAsteroidField(float x, float y, int n, float R)
+    {
+        int i;
+        float r2, dx, dy, phi, scale;
+        GameObject o;
+        float maxScale = 2.0F;
+        float minScale = 0.5F;
+
+        for(i = 0; i < n; i++)
+        {
+            r2 = 2*R*R;
+            dx = 0.0F;
+            dy = 0.0F;
+            while(r2 > R*R)
+            {
+                dx = Random.Range(-R, R);
+                dy = Random.Range(-R, R);
+                r2 = dx*dx + dy*dy;
+            }
+            Vector3 pos = new Vector3(x+dx, y+dy, 0.0F);
+            phi = Random.Range(0.0F,360.0F);
+            Quaternion rot = Quaternion.AngleAxis(phi,Vector3.forward);
+            scale = minScale*Mathf.Pow(maxScale/minScale,Random.Range(0.0F,1.0F));
+
+            o = Instantiate(Asteroid, pos, rot) as GameObject;
+            o.transform.localScale = new Vector3(scale, scale, scale);
+        }
+    }
 }
