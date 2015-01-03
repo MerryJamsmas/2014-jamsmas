07bdd05eaf7dd69cb7257987fc0f493eeeca29b0
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2015-01-02 17:22:06 -0700

    Skeleton Asteroid Field

---
+++ unity/Assets/Scripts/Navigation/AsteroidField.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AsteroidField : MonoBehaviour 
+{
+    public Transform Asteroid;
+
+	// Use this for initialization
+	void Start () 
+	{
+        Debug.Log("Loading Asteroid Field...");
+        Instantiate(Asteroid, new Vector3(1,1,0), Quaternion.identity);
+        Debug.Log("Asteroid Field Loaded.");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+}
